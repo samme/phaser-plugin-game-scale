c3d35bfceecad5ebc78468df179a27fb46d33b45
samme <samme@users.noreply.github.com>
2018-11-17 16:49:18 -0800

    Update examples

--- examples/no-parent.js
+++ examples/no-parent.js
@@ -1,6 +1,7 @@
 var caption1, caption2, caption3, cursor;
 var modes = ['fit', 'resize', 'resize-and-fit', 'none'];
 var colors = window.colors;
+var gui;
 
 window.game = new Phaser.Game({
   width: 640,
@@ -21,7 +22,7 @@ window.game = new Phaser.Game({
         minWidth: 0,
         mode: 'fit',
         resizeCameras: true,
-        snap: null
+        snap: 0
       }
     }]
   },
@@ -32,16 +33,12 @@ window.game = new Phaser.Game({
         .lineStyle(5, 0x01FF70)
         .strokeCircle(320, 320, 320)
         .lineStyle(5, 0x0074D9)
-        .strokeCircle(480, 480, 480);
+        .strokeCircle(640, 640, 640);
 
-      caption1 = this.add.text(0, 0, '', { fill: colors.red, font: '32px monospace' });
-      caption2 = this.add.text(0, 40, '', { fill: colors.yellow, font: '16px monospace', backgroundColor: 'rgba(0,0,0,0.8)' });
-      caption3 = this.add.text(0, 240, '', { fill: colors.orange, font: '16px monospace', backgroundColor: 'rgba(0,0,0,0.8)' });
+      caption1 = this.add.text(0, 0, '', { fill: colors.red, font: '16px monospace', backgroundColor: 'rgba(0,0,0,0.8)' });
+      caption2 = this.add.text(0, 120, '', { fill: colors.yellow, font: '16px monospace', backgroundColor: 'rgba(0,0,0,0.8)' });
+      caption3 = this.add.text(0, 320, '', { fill: colors.orange, font: '16px monospace', backgroundColor: 'rgba(0,0,0,0.8)' });
       cursor = this.add.graphics().lineStyle(3, 0xff00ff).strokeRect(0, 0, 16, 16);
-
-      this.input.on('pointerup', function () {
-        this.gameScale.setMode(modes[(1 + modes.indexOf(this.gameScale.mode)) % modes.length]);
-      }, this);
 
       this.events.on('resize', function (width, height) {
         console.log('Scene "resize" event', width, height);
@@ -50,13 +47,26 @@ window.game = new Phaser.Game({
       this.sys.game.events.on('resize', function (width, height) {
         console.log('Game "resize" event', width, height);
       });
+
+      var setPendingRefresh = this.gameScale.setPendingRefresh.bind(this.gameScale);
+
+      gui = new dat.GUI({ width: 320 });
+      gui.add(this.gameScale, 'debounce');
+      gui.add(this.gameScale, 'debounceDelay', 0, 500, 50);
+      gui.add(this.gameScale, 'maxHeight').onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'maxWidth').onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'minHeight').onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'minWidth').onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'mode', modes).onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'resizeCameras').onChange(setPendingRefresh);
+      gui.add(this.gameScale, 'snap', 0, 100, 10).onChange(setPendingRefresh);
     },
     update: function () {
       var config = this.sys.game.config;
 
-      caption1.setText(config.width + ' ' + config.height);
-      caption2.setText(JSON.stringify(this.gameScale, null, 2));
-      caption3.setText(JSON.stringify(this.gameScale.getBounds(), null, 2));
+      caption1.setText('game.config: ' + JSON.stringify(config, ['width', 'height', 'parent'], 2));
+      caption2.setText('gameScale: ' + JSON.stringify(this.gameScale, null, 2));
+      caption3.setText('bounds: ' + JSON.stringify(this.gameScale.getBounds(), null, 2));
 
       cursor.setPosition(this.input.activePointer.x, this.input.activePointer.y);
     }
